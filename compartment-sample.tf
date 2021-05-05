64f7bb6abed577958d7bd99bb99bbbfa65a7f7bd
Honggo Yeo <honggo@gmail.com>
2021-05-05 10:54:50 +0800

    Removed hard-coded OCID [2021-05-05]

--- compartment-sample.tf
+++ compartment-sample.tf
@@ -1,11 +1,11 @@
 resource "oci_identity_compartment" "tf-compartment-app" {
-    compartment_id = "ocid1.tenancy.oc1..aaaaaaaaopwqdoedjr72up3pg7okzlezsf45yw3nf4j2f5m23kojb5z5j3zq"	
+    compartment_id = "<Tenant_OCID>"	
     description = "Application Compartment"
     name = "Application"
 }
 
 resource "oci_identity_compartment" "tf-compartment-bastion" {
-    compartment_id = "ocid1.tenancy.oc1..aaaaaaaaopwqdoedjr72up3pg7okzlezsf45yw3nf4j2f5m23kojb5z5j3zq"
+    compartment_id = "<Tenant_OCID>"	
     description = "Bastion Compartment"
     name = "Bastion"
 }
---
+++ provider-sample.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid = "<Tenant_OCID>"	
+  user_ocid = "<User_OCID>"	 
+  private_key_path = "<Path_to_API_Private_Key>"
+  fingerprint = "<Fingerprint>"	
+  region = "<Region_ID>"
+}
